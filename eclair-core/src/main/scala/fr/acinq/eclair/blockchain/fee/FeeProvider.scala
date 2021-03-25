a72d8aad5829774d1bc86285765bab9ce8e684b1
wakiyamap <wakiyamap@gmail.com>
2021-03-26 07:25:19 +0900

    Fix minfeerate

--- eclair-core/src/main/scala/fr/acinq/eclair/blockchain/fee/FeeProvider.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/blockchain/fee/FeeProvider.scala
@@ -92,7 +92,7 @@ object FeeratePerKw {
    *
    * See also https://github.com/ElementsProject/lightning/pull/1251
    */
-  val MinimumFeeratePerKw = FeeratePerKw(253 sat)
+  val MinimumFeeratePerKw = FeeratePerKw(25300 sat)
 
   // @formatter:off
   def apply(feeratePerKB: FeeratePerKB): FeeratePerKw = MinimumFeeratePerKw.max(FeeratePerKw(feeratePerKB.feerate / 4))
